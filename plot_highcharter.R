662a46b43805fb48f3f98a1d1f228311e17b8fb6
Andrés Abud <106325634+andyabud@users.noreply.github.com>
2022-10-24 21:03:39 -0300

    Selector de color
    - Incorporación de un selector de color

--- plot_highcharter.R
+++ plot_highcharter.R
@@ -21,6 +21,8 @@ colores_rrss <- c("#4267B2", "#DB4437", "#C13584", "#0072B1", "#1ED760", "#FF000
 data %>% 
 hchart("column", hcaes(x = soporte, y = cantidad_ppto, group = formato)) %>% # dataframe y aes, agrupar por formato
   hc_colors(colores_rrss) %>% # Coloreado del agrupamiento, vector con hex codes creado anteriormente
+  hc_chart( # Parámetros generales del plot
+    backgroundColor = "#FAFAFA" ) %>% # Color del fondo
   hc_title(text = "Inversión Etapa de lanzamiento") %>%  # Título del gráfico
   hc_subtitle(text = "Composición del presupuesto") %>%  # Subtítulo del gráfico
   hc_xAxis( # Parámetros del eje X
@@ -36,13 +38,13 @@ hchart("column", hcaes(x = soporte, y = cantidad_ppto, group = formato)) %>% # d
       style = list(
         fontWeight = "bold", # Tipo de fuente
         fontSize   = "1.4em"))) %>% # Tamaño de fuente
-  hc_plotOptions( # Parámetros del plot que se genera
+  hc_plotOptions( # Parámetros específicos del plot que se genera
     column  = list(
       dataLabels = list(
-        enabled  = TRUE)), # Para habilitar el valor Y arriba de la barra
+        enabled  = TRUE)), # Para habilitar el número arriba de la barra
     series  = list(
       pointWidth = 35)) %>% # Ancho de los elementos de la serie, en este caso las columnas
-  hc_tooltip( # Parámetros del tooltip, el menú que se despliega al pasar el mouse por una columna
+  hc_tooltip(
     backgroundColor = "#FFFFFF", # Color del fondo que se despliega al pasar el tooltip por encima
     headerFormat = "<b>{point.key}</b><br><br>", # Título del tooltip se llama como la variable x (tipo de anuncio)
     pointFormat = "<b>{point.y} %<b/>"# Nombre del punto se llama como la variable y (% ppto)
